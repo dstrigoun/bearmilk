89ac5eb7947bf87f821c474aa95713c4220e6f5c
dstrigoun <dstrigoun@outlook.com>
2021-02-20 23:15:25 -0800

    servers command

--- commands/servers.js
+++ commands/servers.js
@@ -3,7 +3,16 @@ module.exports = {
     description: 'Return SFU related discord servers',
     usage: '[command name]',
     execute(message, args) {
-        // get list of discord servers
-        // invite links to each discord server
+        const Discord = require('discord.js');
+
+        const serverList = new Discord.MessageEmbed()
+            .setTitle("List of SFU Discord servers")
+            .addField("SFU CS^3", "https://discord.gg/FSdAkEUg", false)
+            .addField("SFU", "https://discord.gg/ZyEWFC4", false)
+            .addField("SFU Science", "https://discord.gg/PpaeD63", false)
+            .addField("SFU Economics", "https://discord.gg/yNWfRSR", false)
+            .addField("SFU Engineering", "https://discord.gg/Pyasfh8e", false);
+        
+        message.reply(serverList);
     }
 }
